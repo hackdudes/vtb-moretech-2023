b5a272eb86e3d8bb5dfc00be84975bf9e1a4541d
kemosaaabe <sanchellos27@mail.ru>
2023-10-14 23:18:34 +0300

    Добавил компоненты: - Header - Main - Sidebar - UI Components (Button, Input)

---
+++ frontend/src/layout/Main/index.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/Sidebar";
+
+import styles from "./styles.module.css";
+
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Main;
